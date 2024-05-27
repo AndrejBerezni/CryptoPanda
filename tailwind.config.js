4974874685d1805f7afcde615ae99c0049f32d57
AndrejBerezni <74119993+AndrejBerezni@users.noreply.github.com>
2024-05-27 22:04:25 +0100

    define color scheme and add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#303F9F',
+        secondary: '#90CAF9',
+        accent: '#1A237E',
+        text: '#212121',
+        textDark: '#E0E0E0',
+        textSecondary: '#757575',
+        background: '#F5F5F5',
+        backgroundDark: '#121212',
+        greenAccent: '#4CAF50',
+        redAccent: '#F44336',
+      },
+      fontFamily: {
+        mont: ['Montserrat', 'sans-serif'],
+        source: ['Source Code Pro', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
